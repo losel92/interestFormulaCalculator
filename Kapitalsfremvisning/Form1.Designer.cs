2b542519d078fa8d72e68cf863ad1fda4165580d
Losel Matos <loselmatos@gmail.com>
2019-05-17 20:55:38 +0200

    first commit

---
+++ Kapitalsfremvisning/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace Kapitalsfremvisning
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.kn = new System.Windows.Forms.TextBox();
+            this.k0 = new System.Windows.Forms.TextBox();
+            this.r = new System.Windows.Forms.TextBox();
+            this.n = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.label1.Location = new System.Drawing.Point(43, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 29);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Kn";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.label2.Location = new System.Drawing.Point(179, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 29);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ko";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.label3.Location = new System.Drawing.Point(58, 117);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(21, 29);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "r";
+            // 
+            // kn
+            // 
+            this.kn.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.kn.Location = new System.Drawing.Point(12, 64);
+            this.kn.Name = "kn";
+            this.kn.Size = new System.Drawing.Size(108, 35);
+            this.kn.TabIndex = 3;
+            this.kn.Tag = "group_kn";
+            this.kn.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // k0
+            // 
+            this.k0.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.k0.Location = new System.Drawing.Point(145, 64);
+            this.k0.Name = "k0";
+            this.k0.Size = new System.Drawing.Size(108, 35);
+            this.k0.TabIndex = 4;
+            this.k0.Tag = "group_k0";
+            this.k0.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // r
+            // 
+            this.r.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.r.Location = new System.Drawing.Point(12, 160);
+            this.r.Name = "r";
+            this.r.Size = new System.Drawing.Size(108, 35);
+            this.r.TabIndex = 9;
+            this.r.Tag = "group_r";
+            this.r.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // n
+            // 
+            this.n.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.n.Location = new System.Drawing.Point(145, 160);
+            this.n.Name = "n";
+            this.n.Size = new System.Drawing.Size(108, 35);
+            this.n.TabIndex = 16;
+            this.n.Tag = "group_n";
+            this.n.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.label4.Location = new System.Drawing.Point(184, 117);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(27, 29);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "n";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.button1.Location = new System.Drawing.Point(145, 224);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 35);
+            this.button1.TabIndex = 17;
+            this.button1.Text = "Go!";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.calculateIt);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18.25F);
+            this.button2.Location = new System.Drawing.Point(12, 224);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 35);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Reset";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.resetValues);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(269, 281);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.n);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.r);
+            this.Controls.Add(this.k0);
+            this.Controls.Add(this.kn);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox kn;
+        private System.Windows.Forms.TextBox k0;
+        private System.Windows.Forms.TextBox r;
+        private System.Windows.Forms.TextBox n;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Kapitalsfremvisning/Form1.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Kapitalsfremvisning
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Transforms all the values in the txtBoxes to doubles
+        double k0Num = 0;
+        double rNum = 0;
+        double nNum = 0;
+        double knNum = 0;
+
+        void calculateKn() //Calculates for Kn
+        {
+            knNum = k0Num * Math.Pow(1 + rNum, nNum);
+            kn.Text = Convert.ToString(knNum);
+        }
+        void calculateK0() //Calculates for K0
+        {
+            k0Num = knNum / Math.Pow(1 + rNum, nNum);
+            k0.Text = Convert.ToString(k0Num);
+        }
+        void calculateR() //Calculates for r
+        {
+            rNum = Math.Pow(knNum/k0Num, 1/nNum) - 1;
+            r.Text = Convert.ToString(rNum);
+        }
+        void calculateN() //Calculates for n
+        {
+            nNum = Math.Log(knNum/k0Num) / Math.Log(1 + rNum);
+            n.Text = Convert.ToString(nNum);
+        }
+
+        //Piece of code from StackOverflow
+        //Only accepts numbers and one decimal point in the textboxes
+        private void textBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsControl(e.KeyChar) && !char.IsDigit(e.KeyChar) &&
+                (e.KeyChar != '.'))
+            {
+                e.Handled = true;
+            }
+
+            // only allow one decimal point
+            if ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void calculateIt(object sender, EventArgs e)
+        {
+            //Checks if at least three of the textboxes are filled and gets us the one that isn't (aka the one we need to calculate for)
+            //It also assigns the num doubles to whatever is in the textbox, so long it isn't an empty string
+            int filledCount = 0;
+            var numToFind = k0;
+            if (k0.Text != "") { filledCount++; k0Num = Convert.ToDouble(k0.Text); } else { numToFind = k0; }
+            if (kn.Text != "") { filledCount++; knNum = Convert.ToDouble(kn.Text); } else { numToFind = kn; }
+            if (r.Text != "") { filledCount++; rNum = Convert.ToDouble(r.Text); } else { numToFind = r; }
+            if (n.Text != "") { filledCount++; nNum = Convert.ToDouble(n.Text); } else { numToFind = n; }
+
+            if (filledCount == 3)
+            {
+                if (numToFind == k0) calculateK0();
+                else if (numToFind == kn) calculateKn();
+                else if (numToFind == r) calculateR();
+                else if (numToFind == n) calculateN();
+            }
+            else //If the number of textboxes filled is not equal to three, it will throw an error
+            {
+                MessageBox.Show("Error", "Please fill out exactly three values", MessageBoxButtons.OK);
+            }
+        }
+
+        //Resets all the textboxes
+        private void resetValues(object sender, EventArgs e)
+        {
+            k0.Text = "";
+            kn.Text = "";
+            r.Text = "";
+            n.Text = "";
+        }
+    }
+}
